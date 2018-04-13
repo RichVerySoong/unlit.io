c965ae9c3e94b3039bb1303a70fa5eba8f702ba2
Ben Botvinick <benbotvinicK@gmail.com>
2018-04-13 00:44:36 -0400

    No

--- learning/c++/test.cpp
+++
@@ -1,9 +0,0 @@
-/* system example : DIR */
-#include <stdio.h>      /* printf */
-#include <stdlib.h>     /* system, NULL, EXIT_FAILURE */
-
-int main ()
-{
-    system("ls");
-    return 0;
-}
--- server/server.js
+++ server/server.js
@@ -1,36 +1,31 @@
 var express = require('express');
-var app     = express();
-var http    = require('http').Server(app);
-var io      = require('socket.io')(http);
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var config  = require('./config.json');
+var config = require('./config.json');
 
 app.use(express.static(__dirname + '/../client'));
 
 
-io.on('connection', function(socket){
-  console.log('user connected to socket ' + socket.id);
-  socket.emit('id', socket.id);
-  socket.on('disconnect', function(){
-    console.log('user ' + socket.id + ' disconnected');
-    io.emit('remove_pl', socket.id);
-  });
-  socket.on('init', function(pl){
-    console.log('recieved init');
-    if (JSON.parse(pl).id != "") {
-      console.log('player initiated: ' + pl);
-      io.emit('init', pl);
-    }
-  });
-  socket.on('update', function(pl){
-    if (JSON.parse(pl).id != "") {
-      console.log("Updating player " + JSON.parse(pl).id);
-      io.emit('update', pl);
-    }
-  });
+io.on('connection', function(socket) {
+    socket.emit('id', socket.id);
+    socket.on('disconnect', function() {
+        io.emit('remove_pl', socket.id);
+    });
+    socket.on('init', function(pl) {
+        if (JSON.parse(pl).id != "") {
+            io.emit('init', pl);
+        }
+    });
+    socket.on('update', function(pl) {
+        if (JSON.parse(pl).id != "") {
+            io.emit('update', pl);
+        }
+    });
 });
 
 var serverPort = process.env.PORT || config.port;
 http.listen(serverPort, function() {
-  console.log("Server is listening on port " + serverPort);
+    console.log("Server is listening on port " + serverPort);
 });
