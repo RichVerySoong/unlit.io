71e392025e575c23c0bb56b49326ae13f655e3f9
Dylan <dylan@Not-Dylan.local>
2018-04-13 17:19:53 -0400

    startonfinfout

--- client/js/member-login.js
+++ client/js/member-login.js
@@ -17,3 +17,17 @@ function memberLogin() {
       document.getElementById("memberLogin").innerHTML = "Member Login";
     }
 }
+$(document).ready(function() {
+  var rules = $('#rules');
+  function runIt() {
+  rules.animate({opacity:'-=1'}, 3000);
+  rules.animate({opacity:'+=1'}, 3000, runIt);
+}
+  runIt();
+});
+// function changeText() {
+//   var place = 0;
+//   var ruleList = ["Move your mouse to turn you character and flashlight", "Hit other ghosts with your flashlight", "Don't get hit by another ghost's flahslight", "Be the last ghost living"];
+//   document.getElementById("rules").innerHTML = "HELLO";
+//   runIt();
+// }
