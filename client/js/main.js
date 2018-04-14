95d501635ad30401f5e02ad0ebce6063bf22dac1
Dylan <dylan@Not-Dylan.local>
2018-04-13 20:37:30 -0400

    mf merge conflicts

--- client/js/main.js
+++ client/js/main.js
@@ -1,31 +1,35 @@
 function memberLogin() {
-    var id = document.getElementById("memberLogin").innerHTML
+    var id = document.getElementById("memberLogin").innerHTML;
     if (id == "Member Login") {
-      var passwordInput = document.createElement("input");
-      passwordInput.type = "text";
-      passwordInput.tabindex = "0";
-      passwordInput.autofocus = true;
-      passwordInput.id = "playerPasswordInput";
-      passwordInput.placeholder = "Enter your password here";
-      $("#playerNameInput").after(passwordInput);
-      playerNameInput.placeholder = "Enter your username here"
-      document.getElementById("memberLogin").innerHTML = "Normal Login";
-    }
-    else {
-      $("#playerPasswordInput").remove();
-      playerNameInput.placeholder = "Enter your nickname here"
-      document.getElementById("memberLogin").innerHTML = "Member Login";
+        var passwordInput = document.createElement("input");
+        passwordInput.type = "text";
+        passwordInput.tabindex = "0";
+        passwordInput.autofocus = true;
+        passwordInput.id = "playerPasswordInput";
+        passwordInput.placeholder = "Enter your password here";
+        $("#playerNameInput").after(passwordInput);
+        playerNameInput.placeholder = "Enter your username here"
+        document.getElementById("memberLogin").innerHTML = "Normal Login";
+    } else {
+        $("#playerPasswordInput").remove();
+        playerNameInput.placeholder = "Enter your nickname here";
+        document.getElementById("memberLogin").innerHTML = "Member Login";
     }
 }
-$(document).ready(function() {
-  var rules = $('#rules');
-  function runIt() {
-  rules.animate({opacity:'+=2'}, 3000);
-  rules.animate({opacity:'-=2'}, 3000, runIt);
-}
-  runIt();
-});
 
+$(document).ready(function() {
+    var rules = $('#rules');
+
+    function runIt() {
+        rules.animate({
+            opacity: '+=2'
+        }, 3000);
+        rules.animate({
+            opacity: '-=2'
+        }, 3000, runIt);
+    }
+    runIt();
+});
 var place = 0;
 var ruleList = ["Click to toggle your flashlight!", "Shine your light on other ghosts!", "Avoid other players' flashlights!", "Welcome to unlit.io!"];
 var interval = setInterval(function(){
@@ -37,3 +41,12 @@ document.getElementById("startButton").addEventListener("click", function(){
 })
 
 window.onload = changeText()
+function changeText() {
+    var place = 0;
+    var ruleList = ["Click to toggle your flashlight!", "Shine your light on other ghosts!", "Avoid other players' flashlights!"];
+    setInterval(function(changeText) {
+        document.getElementById("rules").innerHTML = ruleList[place % ruleList.length]
+        place += 1
+    }, 6000);
+}
+window.onload = changeText();
