57864782a967d65b62f90444e80414756286a208
RichVerySoong <35604166+RichVerySoong@users.noreply.github.com>
2018-04-16 18:20:58 -0400

    Update main.js
    Disabled Browser Zoom

--- client/js/main.js
+++ client/js/main.js
@@ -17,6 +17,29 @@ function memberLogin() {
     }
 }
 
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+ });
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+
+$(document).keydown(function(event) {
+if (event.metaKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+  });
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.metaKey == true) {
+       event.preventDefault();
+       }
+
 $(document).ready(function() {
     var rules = $("#rules");
 
